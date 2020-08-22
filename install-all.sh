5bc349efd6e657d71391c324a71f34707a064d34
Brad Osgood <bosgood@gmail.com>
2020-08-22 17:29:08 -0400

    Customize for my own purposes

--- install-all.sh
+++ install-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://raw.github.com/emin/WebPQuickLook/master/WebpQuickLook.tar.gz | tar -xvz
+curl -L https://raw.github.com/bosgood/WebPQuickLook/master/WebpQuickLook.tar.gz | tar -xvz
 sudo mkdir -p /Library/QuickLook/
 sudo mv WebpQuickLook.qlgenerator /Library/QuickLook/
 qlmanage -r
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://raw.github.com/emin/WebPQuickLook/master/WebpQuickLook.tar.gz | tar -xvz
+curl -L https://raw.github.com/bosgood/WebPQuickLook/master/WebpQuickLook.tar.gz | tar -xvz
 mkdir -p ~/Library/QuickLook/
 mv WebpQuickLook.qlgenerator ~/Library/QuickLook/
 qlmanage -r
